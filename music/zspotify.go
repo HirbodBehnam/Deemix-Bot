f15042a22605717b7f2b3b0b7fa4f042f1dc7677
HirbodBehnam <chrome.hiri.angry@gmail.com>
2021-12-02 19:52:32 +0330

    Update to maintain compatibility for ZSpotify

--- music/zspotify.go
+++ music/zspotify.go
@@ -3,7 +3,6 @@ package music
 import (
 	"Deemix-Bot/config"
 	"bytes"
-	"encoding/json"
 	"io/ioutil"
 	"log"
 	"os"
@@ -23,17 +22,14 @@ func (ZSpotify) Download(u string) (*TempDir, error) {
 	}
 	result := &TempDir{Address: dirName}
 	// Add the files to this path needed for zspotify
-	configBytes, _ := json.Marshal(zspotifyConfig{
-		RootPath:        dirName,
-		RootPodcastPath: dirName,
-		DownloadFormat:  "mp3",
-		AntiBanWaitTime: 10, // you can change this value if you want
-		ChunkSize:       50000,
-	})
-	_ = os.WriteFile(path.Join(dirName, "credentials.json"), config.Config.ZSpotifyCredentials, 0666)
-	_ = os.WriteFile(path.Join(dirName, "zs_config.json"), configBytes, 0666)
+	credentialPath := path.Join(dirName, "credentials.json")
+	_ = os.WriteFile(credentialPath, config.Config.ZSpotifyCredentials, 0666)
 	// Download the file
-	cmd := exec.Command("zspotify", u)
+	cmd := exec.Command("zspotify",
+		"--download-format", "mp3",
+		"--root-path", dirName,
+		"--credentials-location", credentialPath,
+		u)
 	cmd.Dir = dirName
 	var stderr bytes.Buffer
 	cmd.Stderr = &stderr
@@ -45,17 +41,4 @@ func (ZSpotify) Download(u string) (*TempDir, error) {
 	}
 	// Return the directory
 	return result, nil
-}
-
-type zspotifyConfig struct {
-	RootPath          string `json:"ROOT_PATH"`
-	RootPodcastPath   string `json:"ROOT_PODCAST_PATH"`
-	SkipExistingFiles bool   `json:"SKIP_EXISTING_FILES"`
-	DownloadFormat    string `json:"DOWNLOAD_FORMAT"`
-	ForcePremium      bool   `json:"FORCE_PREMIUM"`
-	AntiBanWaitTime   int    `json:"ANTI_BAN_WAIT_TIME"`
-	OverrideAutoWait  bool   `json:"OVERRIDE_AUTO_WAIT"`
-	ChunkSize         int    `json:"CHUNK_SIZE"`
-	SplitAlbumDiscs   bool   `json:"SPLIT_ALBUM_DISCS"`
-	DownloadRealTime  bool   `json:"DOWNLOAD_REAL_TIME"`
 }
